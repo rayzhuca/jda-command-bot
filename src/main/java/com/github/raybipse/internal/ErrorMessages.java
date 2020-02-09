cf052f1defb2b42a58d3c3b25356b2975e1b7bba
Ray Z <raybipse@gmail.com>
2020-02-08 20:22:48 -0800

    Added error message methods.

--- src/main/java/com/github/raybipse/internal/ErrorMessages.java
+++ src/main/java/com/github/raybipse/internal/ErrorMessages.java
@@ -3,16 +3,42 @@ package com.github.raybipse.internal;
 import java.util.Objects;
 
 /**
- * ErrorMessages class is responsible for throwing errors inside the java-command-bot framework.
+ * ErrorMessages class is responsible for throwing errors inside the
+ * java-command-bot framework.
+ * 
+ * @author RayBipse
  */
 public class ErrorMessages {
 
-    private ErrorMessages() {}
+    private ErrorMessages() {
+    }
 
+    /**
+     * Returns {@code obj} if it is not null. Throws a {@link NullPointerException}
+     * otherwise.
+     * 
+     * @param <T>       the type of {@code obj}
+     * @param obj       the obj to be checked for nullity
+     * @param paramName the name of the {@code obj}
+     * @return {@code obj}
+     */
     public static <T> T requireNonNullParam(T obj, String paramName) {
         return Objects.requireNonNull(obj, "Parameter \"" + paramName + "\" should not be null.");
     }
 
+    /**
+     * Returns {@code obj} if it is not null. Throws a {@link NullPointerException}
+     * otherwise.
+     * 
+     * <pre>
+     * requireNonNullReturn(method(), "method");
+     * </pre>
+     * 
+     * @param <T>        the type of {@code obj}
+     * @param obj        the obj to be checked for nullity
+     * @param methodName the name of the {@code obj}
+     * @return {@code obj}
+     */
     public static <T> T requireNonNullReturn(T obj, String methodName) {
         return Objects.requireNonNull(obj, "Method \"" + methodName + "\" should not return null.");
     }
