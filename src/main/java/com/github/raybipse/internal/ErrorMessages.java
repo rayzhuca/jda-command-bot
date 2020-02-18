42eacb1f6d13d9e2fd587a9e4d398e9eb27d2eaa
Ray Z <raybipse@gmail.com>
2020-02-17 19:50:07 -0800

    Added requireNonNull().

--- src/main/java/com/github/raybipse/internal/ErrorMessages.java
+++ src/main/java/com/github/raybipse/internal/ErrorMessages.java
@@ -5,22 +5,42 @@ import java.util.Objects;
 
 /**
  * ErrorMessages class is responsible for throwing errors inside the
- * java-command-bot framework.
+ * jda-command-bot framework.
  * 
  * @author RayBipse
  */
 public class ErrorMessages {
 
+    /**
+     * BotConfiguration cannot be instantized because it is a utility class.
+     */
     private ErrorMessages() {
     }
 
+    
     /**
-     * Returns {@code obj} if it is not null. Throws a {@link NullPointerException}
-     * otherwise.
+     * Returns {@code obj} if it is not {@code null}. Otherwise, throw a {@link NullPointerException}.
+     * 
+     * @param <T> the type of {@code obj}
+     * @param obj the obj to be checked for nullity
+     * @param msg the message of the {@link NullPointerException}
+     * 
+     * @return {@code obj}
+]     */
+    public static <T> T requireNonNull(T obj, String msg) {
+        if (obj != null) {
+            return obj;
+        }
+        throw new NullPointerException(msg);
+    }
+
+    /**
+     * Returns {@code obj} if it is not {code null}. Otherwise, throw a {@link NullPointerException}.
      * 
      * @param <T>       the type of {@code obj}
      * @param obj       the obj to be checked for nullity
      * @param paramName the name of the {@code obj}
+     * 
      * @return {@code obj}
      */
     public static <T> T requireNonNullParam(T obj, String paramName) {
@@ -31,8 +51,7 @@ public class ErrorMessages {
     }
 
     /**
-     * Returns {@code obj} if it is not null. Throws a {@link NullPointerException}
-     * otherwise.
+     * Returns {@code obj} if it is not {code null}. Otherwise, throw a {@link NullPointerException}.
      * 
      * <pre>
      * requireNonNullReturn(method(), "method");
@@ -41,6 +60,7 @@ public class ErrorMessages {
      * @param <T>        the type of {@code obj}
      * @param obj        the obj to be checked for nullity
      * @param methodName the name of the {@code obj}
+     * 
      * @return {@code obj}
      */
     public static <T> T requireNonNullReturn(T obj, String methodName) {
